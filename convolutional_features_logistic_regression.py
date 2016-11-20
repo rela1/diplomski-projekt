2bf839bef4da62507b3ad0fb683373089bc84125
rela1 <ivan.relic1@gmail.com>
2016-11-20 02:01:16 +0100

    bug fix

--- convolutional_features_logistic_regression.py
+++ convolutional_features_logistic_regression.py
@@ -9,7 +9,7 @@ import os
 from sklearn import metrics
 import logreg
 
-EPOCHS = 2000
+EPOCHS = 100
 LEARNING_RATE = 1e-4
 WEIGHT_DECAY = 1e-4
 
@@ -29,10 +29,9 @@ if __name__ == '__main__':
 	X_test = np.concatenate((X_test_left, X_test_middle), axis=1)
 	y_test = dataset.read_labels(sys.argv[1], 'test')
 	y_test_oh = np.array([[1 if y_test[i] == j else 0 for j in range(2)] for i in range(len(y_test))])
-	"""
 	best_acc = 0
 	best_lambda = 0
-	for lambda_factor in np.linspace(2**-4, 2**-1, num=5):
+	for lambda_factor in np.linspace(2**-7, 2**-1, num=20):
 		model = logreg.TFLogReg(X_train.shape[1], 2, param_delta=LEARNING_RATE, param_lambda=lambda_factor)
 		model.train(X_train, y_train_oh, EPOCHS)
 		y_validate_pred = model.eval(X_validate)
@@ -40,11 +39,10 @@ if __name__ == '__main__':
 		if acc > best_acc:
 			best_acc = acc
 			best_lambda = lambda_factor
-	"""
 	X_train = np.append(X_train, X_validate, axis=0)
 	y_train = np.append(y_train, y_validate, axis=0)
 	y_train_oh = np.append(y_train_oh, y_validate_oh, axis=0)
-	model = logreg.TFLogReg(X_train.shape[1], 2, param_delta=LEARNING_RATE, param_lambda=WEIGHT_DECAY)
+	model = logreg.TFLogReg(X_train.shape[1], 2, param_delta=LEARNING_RATE, param_lambda=best_lambda)
 	model.train(X_train, y_train_oh, EPOCHS)
 	y_train_pred = model.eval(X_train)
 	y_train_pred = np.argmax(y_train_pred, axis=1)
@@ -63,7 +61,7 @@ if __name__ == '__main__':
 	print("\taverage precision: ", metrics.average_precision_score(y_test, y_test_pred))
 	print("\trecall: ", metrics.recall_score(y_test, y_test_pred))
 	X_test_imgs = dataset.read_images(sys.argv[1], 'test')
-	if sys.argv[4]:
+	if len(sys.argv > 4):
 		misclassified_output_folder = sys.argv[4]
 		for index, image in enumerate(X_test_imgs):
 			if y_test_pred[index] != y_test[index]:
