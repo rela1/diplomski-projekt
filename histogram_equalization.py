573d42bd0a03448c6e7f176fa48bc0b4433f6a42
rela1 <ivan.relic1@gmail.com>
2016-11-28 01:35:00 +0100

    histogram adaptive equalization

--- histogram_equalization.py
+++ histogram_equalization.py
@@ -9,4 +9,6 @@ if __name__ == '__main__':
 		image_name = os.path.basename(img_path)
 		image = mpimg.imread(img_path)
 		image_normalized = exposure.equalize_adapthist(image, clip_limit=0.03)
+		assert image_normalized.shape == image.shape
+		assert image_normalized.shape[-1] == 3
 		mpimg.imsave(os.path.join(output_folder, image_name), image_normalized)
