cd30ea6fdc84eb7cfcc99212154fc67cb1ee146e
rela1 <ivan.relic1@gmail.com>
2016-12-04 23:51:30 +0100

    added pooled convolutional extractor

--- models/vgg_vertically_sliced.py
+++ models/vgg_vertically_sliced.py
@@ -108,7 +108,7 @@ def build_convolutional_feature_extractor(inputs, weight_decay, vgg_init_dir, is
     last_convolution_filter_size = net.get_shape()
     net = layers.max_pool2d(net, kernel_size=[int(last_convolution_filter_size[1]), 1], stride=1)
     slice_size = int(int(last_convolution_filter_size[2]) / 3)
-    net = tf.slice(net, begin=[0, 0, vertical_slice * slice_size, 0], size=[-1, -1, slice_size, -1])
+    net = tf.slice(net, begin=[0, 0, 0, 0], size=[-1, -1, slice_size * 2, -1])
     print("Created slice from ", [0, 0, 0, 0], "with size ", [-1, -1, slice_size * 2, -1])
 
     net = tf.contrib.layers.flatten(net, scope='flatten')
