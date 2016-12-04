d7d7b8551a67950e11b22b520bc9e50067caba46
rela1 <ivan.relic1@gmail.com>
2016-12-04 23:50:02 +0100

    added pooled convolutional extractor

--- models/vgg_vertically_sliced.py
+++ models/vgg_vertically_sliced.py
@@ -105,12 +105,11 @@ def build_convolutional_feature_extractor(inputs, weight_decay, vgg_init_dir, is
 
     net = layers.convolution2d(net, 4096, kernel_size=7, scope='conv6_1')
     
-    if vertical_slice != None:
-      last_convolution_filter_size = net.get_shape()
-      net = layers.max_pool2d(net, kernel_size=[int(last_convolution_filter_size[1]), 1], stride=1)
-      slice_size = int(int(last_convolution_filter_size[2]) / 3)
-      net = tf.slice(net, begin=[0, 0, vertical_slice * slice_size, 0], size=[-1, -1, slice_size, -1])
-      print("Created slice from ", [0, 0, 0, 0], "with size ", [-1, -1, slice_size * 2, -1])
+    last_convolution_filter_size = net.get_shape()
+    net = layers.max_pool2d(net, kernel_size=[int(last_convolution_filter_size[1]), 1], stride=1)
+    slice_size = int(int(last_convolution_filter_size[2]) / 3)
+    net = tf.slice(net, begin=[0, 0, vertical_slice * slice_size, 0], size=[-1, -1, slice_size, -1])
+    print("Created slice from ", [0, 0, 0, 0], "with size ", [-1, -1, slice_size * 2, -1])
 
     net = tf.contrib.layers.flatten(net, scope='flatten')
 
