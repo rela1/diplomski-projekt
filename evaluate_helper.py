b56e0009bc9cd7e9fb844e81e054a589b0a6e67c
rela1 <ivan.relic1@gmail.com>
2016-12-05 17:43:26 +0100

    refactoring

--- evaluate_helper.py
+++ evaluate_helper.py
@@ -1,5 +1,14 @@
+from sklearn import metrics
+
+METRIC_FUNCTIONS = (metrics.accuracy_score, metrics.precision_score, metrics.average_precision_score, metrics.recall_score)
+
+def print_metrics(metrics):
+  for metric in metrics:
+    print('\t{}={}'.format(metric, metrics[metric]))
+
 def evaluate_metric_functions(y_true, y_pred, metric_functions):
-	"""Evaluates given metric functions on given true and predicted dense data.
+	"""
+  Evaluates given metric functions on given true and predicted dense data.
 
 	Keyword arguments:
 	y_true -- true densely stored classes
@@ -16,33 +25,34 @@ def tf_predict_func(sess, inputs, logits):
 		return np.argmax(sess.run(logits, feed_dict={inputs:x}))
 	return predict_func
 
-def evaluate(name, x, y, predict_function):
+def evaluate(name, x, y, batch_size, predict_function):
+  """
+  Evaluates given predict function on given data in batches.
+
+  Keyword arguments:
+  name -- name of evaluation (train, test, valid, etc.)
+  x -- data to be predicted
+  y -- true densely stored classes
+  batch_size -- size of evaluate batch
+  predict_function -- function used for prediction; called as predict_function(x) on batch of data x
+
+  Returns:
+  dictionary mapping metric function name to metric score on given data with given predict function
+  """
   print("\nRunning evaluation: ", name)
-  batch_size = BATCH_SIZE
   num_examples = x.shape[0]
-  conf_matrix = np.zeros((2, 2))
   assert num_examples % batch_size == 0
   num_batches = num_examples // batch_size
-  loss_avg = 0
+  y_predict = []
   for i in range(num_batches):
     batch_x = x[i*batch_size:(i+1)*batch_size, :]
     batch_y = y[i*batch_size:(i+1)*batch_size, :]
-    logits_val= session.run([logits, loss], feed_dict={inputs:batch_x, labels:batch_y})
-    yp = np.argmax(logits_val, 1)
-    yt = np.argmax(batch_y, 1)
-    conf_matrix_batch = confusion_matrix(yt, yp, labels=np.arange(2))
-    np.add(conf_matrix, conf_matrix_batch, conf_matrix)
-    loss_avg += loss_val
-  loss_avg /= num_batches
-  total_conf_matrix_sum = np.sum(conf_matrix)
-  row_conf_matrix_sum = np.sum(conf_matrix, axis = 1)
-  column_conf_matrix_sum = np.sum(conf_matrix, axis = 0)
-  diagonal_conf_matrix_sum = np.sum(np.diag(conf_matrix))
-  acc = diagonal_conf_matrix_sum / total_conf_matrix_sum
-  prec = [conf_matrix[i][i] / column_conf_matrix_sum[i] for i in range(2)]
-  rec = [conf_matrix[i][i] / row_conf_matrix_sum[i] for i in range(2)]
-  print(name + " accuracy = %.2f" % acc)
-  print(name + " per class precision = %s" % prec)
-  print(name + " per class recall = %s" % rec)
-  print(name + " avg loss = %.2f\n" % loss_avg)
-  return loss_avg, acc
+    start_time = time.time()
+    predict_batch_y = predict_function(batch_x)
+    duration = time.time() - start_time
+    y_predict.extend(predict_batch_y)
+    if (i+1) % 10 == 0:
+      print('step {}/{}, {} examples/sec, {} sec/batch'.format(i, num_batches, batch_size / duration, duration))
+  metrics = evaluate_metric_functions(y, y_predict, METRIC_FUNCTIONS)
+  print_metrics(metrics)
+  return metrics
