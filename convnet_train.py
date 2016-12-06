26a3b76c8361ff5bfea365d4983019dddb6b3817
rela1 <ivan.relic1@gmail.com>
2016-12-06 20:31:48 +0100

    refactoring

--- convnet_train.py
+++ convnet_train.py
@@ -12,9 +12,9 @@ import evaluate_helper
 np.set_printoptions(linewidth=250)
 
 BATCH_SIZE = 10
-WEIGHT_DECAY = 1e-4
+WEIGHT_DECAY = 1e-3
 LEARNING_RATE = 1e-4
-FULLY_CONNECTED = [200]
+FULLY_CONNECTED = [2000, 20]
 NUM_CLASSES = 2
 EPOCHS = 150
 
@@ -38,7 +38,7 @@ def train(model, vgg_init_dir, dataset_root, model_path):
     with tf.variable_scope('model', reuse=True):
       logits_eval, loss_eval = model.build(data_node, labels_node, NUM_CLASSES, fully_connected=FULLY_CONNECTED, is_training=False)
 
-    exponential_learning_rate = tf.train.exponential_decay(LEARNING_RATE, global_step, 200, 0.99, staircase=True)
+    exponential_learning_rate = tf.train.exponential_decay(LEARNING_RATE, global_step, 200, 0.9, staircase=True)
     opt = tf.train.AdamOptimizer(exponential_learning_rate)
     grads = opt.compute_gradients(loss)
     apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
