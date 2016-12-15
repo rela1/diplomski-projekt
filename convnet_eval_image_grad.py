94952f75c510b04a5a7d11e17d8fcf8db1c621dc
rela1 <ivan.relic1@gmail.com>
2016-12-15 19:56:21 +0100

    added images gradient calculation

--- convnet_eval_image_grad.py
+++ convnet_eval_image_grad.py
@@ -46,21 +46,22 @@ def evaluate(model, dataset_root, images_root, model_path, misclassified_output_
 
     test_images = dataset.read_images(images_root, 'test')
 
-  for index, image in enumerate(test_images):
-    if test_labels_pred[index] != test_labels[index]:
-      img.imsave(os.path.join(misclassified_output_folder, str(test_labels[index]) + "_" + str(index) + ".png"), image)
+    for index, image in enumerate(test_images):
+      if test_labels_pred[index] != test_labels[index]:
+        img.imsave(os.path.join(misclassified_output_folder, str(test_labels[index]) + "_" + str(index) + ".png"), image)
 
-  num_batches = len(test_data) // BATCH_SIZE
-  img_index = 0
-  for i in range(num_batches):
-    batch_images = test_data[i*BATCH_SIZE:(i+1)*BATCH_SIZE, :]
-    batch_labels = test_labels[i*BATCH_SIZE:(i+1)*BATCH_SIZE]
-    grads_operation = tf.gradients(logits_eval, [data_node])[0]
-    batch_images_grads = sess.run(grads_operation, feed_dict={data_node:batch_images})
-    for index, batch_image_grads, image in enumerate(zip(batch_images_grads, batch_images)):
-      img.imsave(os.path.join(image_gradients_output_folder, str(batch_labels[index]) + "_" + str(img_index) + ".png"), image)
-      img.imsave(os.path.join(image_gradients_output_folder, str(batch_labels[index]) + "_" + str(img_index) + "_gradients.png"), batch_image_grads)
-      img_index += 1
+    num_batches = len(test_data) // BATCH_SIZE
+    img_index = 0
+    for i in range(num_batches):
+      batch_data = test_data[i*BATCH_SIZE:(i+1)*BATCH_SIZE, :]
+      batch_images = test_images[i*BATCH_SIZE:(i+1)*BATCH_SIZE, :]
+      batch_labels = test_labels[i*BATCH_SIZE:(i+1)*BATCH_SIZE]
+      grads_operation = tf.gradients(logits_eval, [data_node])[0]
+      batch_images_grads = sess.run(grads_operation, feed_dict={data_node:batch_data})
+      for index in range(len(batch_images)):
+        img.imsave(os.path.join(image_gradients_output_folder, str(batch_labels[index]) + "_" + str(img_index) + ".png"), batch_images[index])
+        img.imsave(os.path.join(image_gradients_output_folder, str(batch_labels[index]) + "_" + str(img_index) + "_gradients.png"), batch_images_grads[index])
+        img_index += 1
 
 if __name__ == '__main__':
   dataset_root = sys.argv[1]
