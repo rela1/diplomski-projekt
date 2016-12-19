ff9b664f6c448ee3f920d95e70e8175c66c2b880
rela1 <ivan.relic1@gmail.com>
2016-12-19 10:03:31 +0100

    train parameters changed

--- convnet_train.py
+++ convnet_train.py
@@ -38,8 +38,8 @@ def train(model, vgg_init_dir, dataset_root, model_path):
     with tf.variable_scope('model', reuse=True):
       logits_eval, loss_eval = model.build(data_node, labels_node, NUM_CLASSES, fully_connected=FULLY_CONNECTED, is_training=False)
 
-    #exponential_learning_rate = tf.train.exponential_decay(LEARNING_RATE, global_step, 200, 0.94, staircase=True)
-    opt = tf.train.AdamOptimizer(LEARNING_RATE)
+    exponential_learning_rate = tf.train.exponential_decay(LEARNING_RATE, global_step, 4000, 0.94, staircase=True)
+    opt = tf.train.AdamOptimizer(exponential_learning_rate)
     grads = opt.compute_gradients(loss)
     apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
     with tf.control_dependencies([apply_gradient_op]):
