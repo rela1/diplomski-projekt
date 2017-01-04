f77f6cd7996102b8bd1655c6e7a2fbee3b398f3f
rela1 <ivan.relic1@gmail.com>
2017-01-04 12:50:47 +0100

    added scaled convnet

---
+++ convnet_train_scales.py
@@ -0,0 +1,93 @@
+import os
+import sys
+import time
+
+import numpy as np
+import tensorflow as tf
+
+import attributes_dataset as dataset
+from models import vgg_vertically_sliced
+import evaluate_helper
+
+np.set_printoptions(linewidth=250)
+
+BATCH_SIZE = 10
+WEIGHT_DECAY = 1e-3
+LEARNING_RATE = 1e-4
+FULLY_CONNECTED = [200]
+NUM_CLASSES = 2
+EPOCHS = 150
+
+def train(model, vgg_init_dir, dataset_root, model_path):
+  train_data, train_labels, validate_data, validate_labels, test_data, test_labels = dataset.read_and_normalize_images(dataset_root)
+
+  train_size = train_data.shape[0]
+
+  with tf.Graph().as_default():
+    sess = tf.Session()
+    global_step = tf.get_variable('global_step', [], dtype=tf.int64,
+        initializer=tf.constant_initializer(0), trainable=False)
+    num_batches_per_epoch = train_size // BATCH_SIZE
+
+    data_node = tf.placeholder(tf.float32,
+        shape=(BATCH_SIZE, train_data.shape[1], train_data.shape[2], train_data.shape[3]))
+    labels_node = tf.placeholder(tf.int64, shape=(BATCH_SIZE,))
+
+    with tf.variable_scope('model'):      
+      logits, loss, init_op, init_feed = model.build_scaled(data_node, labels_node, NUM_CLASSES, scales=[1,2], width_tiles=7, height_tiles=2, fully_connected=FULLY_CONNECTED, weight_decay=WEIGHT_DECAY, vgg_init_dir=vgg_init_dir, is_training=True)
+    with tf.variable_scope('model', reuse=True):
+      logits_eval, loss_eval = model.build_scaled(data_node, labels_node, NUM_CLASSES, scales=[1,2], width_tiles=7, height_tiles=2, fully_connected=FULLY_CONNECTED, is_training=False)
+
+    exponential_learning_rate = tf.train.exponential_decay(LEARNING_RATE, global_step, 2000, 0.5, staircase=True)
+    opt = tf.train.AdamOptimizer(exponential_learning_rate)
+    grads = opt.compute_gradients(loss)
+    apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+    with tf.control_dependencies([apply_gradient_op]):
+      train_op = tf.no_op(name='train')
+
+    sess.run(tf.initialize_all_variables())
+    sess.run(tf.initialize_local_variables())
+    sess.run(init_op, feed_dict=init_feed)
+
+    ex_start_time = time.time()
+    num_batches = train_size // BATCH_SIZE
+    global_step_val = 0
+    best_accuracy = 0
+    saver = tf.train.Saver()
+    for epoch_num in range(1, EPOCHS + 1):
+      indices = np.arange(train_size)
+      np.random.shuffle(indices)
+      train_data = np.ascontiguousarray(train_data[indices])
+      train_labels = np.ascontiguousarray(train_labels[indices])
+      for step in range(num_batches):
+        offset = step * BATCH_SIZE 
+        batch_data = train_data[offset:(offset + BATCH_SIZE), ...]
+        batch_labels = train_labels[offset:(offset + BATCH_SIZE)]
+        feed_dict = {data_node: batch_data, labels_node: batch_labels}
+        start_time = time.time()
+        run_ops = [train_op, loss, logits, global_step]
+        _, loss_val, scores, global_step_val = sess.run(run_ops, feed_dict=feed_dict)
+        duration = time.time() - start_time
+        assert not np.isnan(loss_val), 'Model diverged with loss = NaN'
+        if (step+1) % 5 == 0:
+          num_examples_per_step = BATCH_SIZE
+          examples_per_sec = num_examples_per_step / duration
+          sec_per_batch = float(duration)
+          format_str = 'epoch %d, batch %d / %d, loss = %.2f \
+            (%.1f examples/sec; %.3f sec/batch)'
+          print(format_str % (epoch_num, step+1, num_batches, loss_val, examples_per_sec, sec_per_batch))
+      valid_accuracy = evaluate_helper.evaluate('validate', validate_data, validate_labels, BATCH_SIZE, 
+      	evaluate_helper.tf_predict_func(sess, data_node, logits_eval), verbose=True)[0]['accuracy_score']
+      if valid_accuracy > best_accuracy:
+        best_accuracy = valid_accuracy
+        saver.save(sess, model_path)
+      print('Best validate accuracy = %.2f' % best_accuracy)
+    saver.restore(sess, model_path)
+    evaluate_helper.evaluate('test', test_data, test_labels, BATCH_SIZE, 
+      	evaluate_helper.tf_predict_func(sess, data_node, logits_eval), verbose=True)
+
+if __name__ == '__main__':
+  vgg_init_dir = sys.argv[1]
+  dataset_root = sys.argv[2]
+  model_path = sys.argv[3]
+  train(vgg_vertically_sliced, vgg_init_dir, dataset_root, model_path)
--- models/vgg_vertically_sliced.py
+++ models/vgg_vertically_sliced.py
@@ -26,6 +26,75 @@ def create_init_op(vgg_layers):
   init_op, init_feed = tf.contrib.framework.assign_from_values(init_map)
   return init_op, init_feed
 
+def build_convolutional_scaled_pooled_feature_extractor(inputs, scales=[1, 2], width_tiles=7, height_tiles=2, weight_decay=0.0, vgg_init_dir=None, is_training=True):
+  if is_training:
+    vgg_layers, vgg_layer_names = read_vgg_init(vgg_init_dir)
+
+  output_maps = []
+
+  for scale in scales:
+    inputs_shape = inputs.get_shape()
+    inputs_shape[1] = inputs_shape[1] / scale
+    inputs_shape[2] = inputs_shape[2] / scale
+    final_map_width = inputs_shape[2] // 32
+    final_map_height = inputs_shape[1] // 32
+    if final_map_height < height_tiles or final_map_width < width_tiles:
+        raise Exception('Tiles (width, height) is :{}, final output map (width, height) on scale {} is: {}'.format((width_tiles, height_tiles), scale, (final_map_width, final_map_height)))
+
+  with tf.variable_scope('operations', reuse=True):
+    for scale in scales:
+        inputs_shape = inputs.get_shape()
+        if scale not 1:
+            scaled_inputs = tf.image.resize_images(inputs, [inputs_shape[0], inputs_shape[1] / scale, inputs_shape[2] / scale, inputs_shape[3]])
+        else:
+            scaled_inputs = inputs
+        scaled_inputs_shape = scaled_inputs.get_shape()
+        horizontal_slice_size = int(round(int(scaled_inputs_shape[2]) / 3))
+        vertical_slice_size = int(round(int(scaled_inputs_shape[1]) / 3))
+        scaled_inputs = tf.slice(scaled_inputs, begin=[0, vertical_slice_size, 0, 0], size=[-1, vertical_slice_size, horizontal_slice_size * 2, -1])
+
+        with tf.contrib.framework.arg_scope([layers.convolution2d],
+              kernel_size=3, stride=1, padding='SAME', rate=1, activation_fn=tf.nn.relu,
+              normalizer_fn=None, weights_initializer=None,
+              weights_regularizer=layers.l2_regularizer(weight_decay)):
+
+            net = layers.convolution2d(inputs, 64, scope='conv1_1')
+            net = layers.convolution2d(net, 64, scope='conv1_2')
+            net = layers.max_pool2d(net, 2, 2, scope='pool1')
+            net = layers.convolution2d(net, 128, scope='conv2_1')
+            net = layers.convolution2d(net, 128, scope='conv2_2')
+            net = layers.max_pool2d(net, 2, 2, scope='pool2')
+            net = layers.convolution2d(net, 256, scope='conv3_1')
+            net = layers.convolution2d(net, 256, scope='conv3_2')
+            net = layers.convolution2d(net, 256, scope='conv3_3')
+            net = layers.max_pool2d(net, 2, 2, scope='pool3')
+            net = layers.convolution2d(net, 512, scope='conv4_1')
+            net = layers.convolution2d(net, 512, scope='conv4_2')
+            net = layers.convolution2d(net, 512, scope='conv4_3')
+            net = layers.max_pool2d(net, 2, 2, scope='pool4')
+            net = layers.convolution2d(net, 512, scope='conv5_1')
+            net = layers.convolution2d(net, 512, scope='conv5_2')
+            net = layers.convolution2d(net, 512, scope='conv5_3')
+            net = layers.max_pool2d(net, 2, 2, scope='pool5')
+
+            net_shape = net.get_shape()
+            pool_height = int(round(net_shape[1] / height_tiles))
+            pool_width = int(round(net_shape[2] / width_tiles))
+            kernel = [pool_height, pool_width]
+            net = layers.max_pool2d(net, kernel_size=kernel, stride=kernel)
+            output_maps.append(net)
+      
+    packed_output_maps = tf.pack(output_maps)
+    averaged_output_maps = tf.reduce_mean(packed_output_maps, reduction_indices=0)
+
+    final_output = tf.contrib.layers.flatten(averaged_output_maps, scope='flatten')
+    
+    if is_training:
+          init_op, init_feed = create_init_op(vgg_layers)
+          return final_output, init_op, init_feed
+
+    return final_output
+
 def build_convolutional_pooled_feature_extractor(inputs, weight_decay=0.0, vgg_init_dir=None, is_training=True):
   if is_training:
     vgg_layers, vgg_layer_names = read_vgg_init(vgg_init_dir)
@@ -123,6 +192,38 @@ def build_convolutional_feature_extractor(inputs, weight_decay=0.0, vgg_init_dir
       return net, init_op, init_feed
 
     return net
+
+def build_scaled(inputs, labels, num_classes, scales=[1,2], width_tiles=7, height_tiles=2, fully_connected=[], weight_decay=0.0, vgg_init_dir=None, is_training=True):
+  bn_params = {
+      'decay': 0.999,
+      'center': True,
+      'scale': True,
+      'epsilon': 0.001,
+      'updates_collections': None,
+      'is_training': is_training,
+  }
+
+  if is_training:
+    net, init_op, init_feed = build_convolutional_scaled_pooled_feature_extractor(inputs, scales=scales, width_tiles=width_tiles, height_tiles=height_tiles, weight_decay=weight_decay, vgg_init_dir=vgg_init_dir, is_training=is_training)
+  else:
+    net = build_convolutional_scaled_pooled_feature_extractor(inputs, scales=scales, width_tiles=width_tiles, height_tiles=height_tiles, weight_decay=weight_decay, vgg_init_dir=vgg_init_dir, is_training=is_training)
+
+  with tf.contrib.framework.arg_scope([layers.fully_connected],
+      activation_fn=tf.nn.relu, normalizer_fn=layers.batch_norm, normalizer_params=bn_params,
+      weights_initializer=initializers.xavier_initializer(),
+      weights_regularizer=layers.l2_regularizer(weight_decay)):
+    layer_num = 1
+    for fully_connected_num in fully_connected:
+      net = layers.fully_connected(net, fully_connected_num, scope='fc{}'.format(layer_num))
+      layer_num += 1
+  logits = layers.fully_connected(net, num_classes, activation_fn=None, scope='logits')
+
+  total_loss = loss(logits, labels, is_training)
+  
+  if is_training:
+    return logits, total_loss, init_op, init_feed
+
+  return logits, total_loss   
 
 def build(inputs, labels, num_classes, fully_connected=[], weight_decay=0.0, vgg_init_dir=None, is_training=True):
   bn_params = {
