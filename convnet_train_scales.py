b23df5a4369134bc0537a0dfd1b10e9edbe3ab0e
rela1 <ivan.relic1@gmail.com>
2017-01-22 11:30:51 +0100

    created scripts for labeling folder of images wih convnet

--- convnet_train_scales.py
+++ convnet_train_scales.py
@@ -38,8 +38,8 @@ def train(model, vgg_init_dir, dataset_root, model_path):
     with tf.variable_scope('model', reuse=True):
       logits_eval, loss_eval = model.build_scaled(data_node, labels_node, NUM_CLASSES, scales=[1,2,4], width_tiles=7, height_tiles=2, fully_connected=FULLY_CONNECTED, is_training=False)
 
-    exponential_learning_rate = tf.train.exponential_decay(LEARNING_RATE, global_step, 5000, 0.5, staircase=True)
-    opt = tf.train.AdamOptimizer(exponential_learning_rate)
+    #exponential_learning_rate = tf.train.exponential_decay(LEARNING_RATE, global_step, 5000, 0.5, staircase=True)
+    opt = tf.train.AdamOptimizer(LEARNING_RATE)
     grads = opt.compute_gradients(loss)
     apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
     with tf.control_dependencies([apply_gradient_op]):
