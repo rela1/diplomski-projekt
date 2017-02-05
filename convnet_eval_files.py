2152a1a895aac67d8c752a987279ba4293bef211
rela1 <ivan.relic1@gmail.com>
2017-02-05 13:39:45 +0100

    ignoring errors on transitions

--- convnet_eval_files.py
+++ convnet_eval_files.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
             true = int(f_true.read())
             pred = int(f_pred.read())
             for r in ranges:
-                if abs(r[0] - i) < 20  or abs(r[1] - i) < 20:
+                if abs(r[0] - i) < 25  or abs(r[1] - i) < 25:
                     pred = true
                     break
             if true == 1:
