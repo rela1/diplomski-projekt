5ce629c69675341fe623da3574a73e79d95e26ac
rela1 <ivan.relic1@gmail.com>
2017-03-30 12:54:39 +0200

    python glob

--- convnet_label_images.py
+++ convnet_label_images.py
@@ -69,5 +69,5 @@ if __name__ == '__main__':
   model_input_width = int(sys.argv[4])
   model_input_channels = int(sys.argv[5])
   rgb_mean = np.array(eval(sys.argv[6]))
-  image_paths = glob.glob(sys.argv[7:])
+  image_paths = glob.glob(sys.argv[7])
   label(vgg_vertically_sliced, labels_root_folder, image_paths, model_path, (model_input_height, model_input_width, model_input_channels), rgb_mean)
