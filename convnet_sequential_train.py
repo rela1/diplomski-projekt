6fb6d2a0de3e0f8543f6857e2d2bacfc5734dd01
rela1 <ivan.relic1@gmail.com>
2017-04-04 19:56:17 +0200

    record reader options bug fix

--- convnet_sequential_train.py
+++ convnet_sequential_train.py
@@ -91,7 +91,7 @@ def train(model, vgg_init_dir, dataset_root, model_path):
 
   train_images, train_label = input_decoder(train_file_queue)
   min_after_dequeue = 10000
-  capacity = min_after_dequeue + 3 * batch_size
+  capacity = min_after_dequeue + 3 * 1
   train_images, train_label = tf.train.shuffle_batch(
       [train_images, train_label], batch_size=1, capacity=capacity,
       min_after_dequeue=min_after_dequeue)
