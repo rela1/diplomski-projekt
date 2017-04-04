50f54fd9a6f5c284fe6037ecba0688d76160664d
rela1 <ivan.relic1@gmail.com>
2017-04-04 20:37:59 +0200

    added script for generating datasets

--- models/vgg_vertically_sliced.py
+++ models/vgg_vertically_sliced.py
@@ -207,9 +207,8 @@ def build_convolutional_sequential_feature_extractor(inputs, weight_decay, vgg_i
         vgg_layers, vgg_layer_names = read_vgg_init(vgg_init_dir)
 
     inputs_shape = inputs.get_shape()
-    constant_3 = tf.constant(3)
-    horizontal_slice_size = tf.cast(tf.round(tf.divide(inputs_shape[2], constant_3)), tf.int32)
-    vertical_slice_size = tf.cast(tf.round(tf.divide(inputs_shape[1], constant_3)), tf.int32)
+    horizontal_slice_size = int(round(int(inputs_shape[2]) / 3))
+    vertical_slice_size = int(round(int(inputs_shape[1]) / 3))
     inputs = tf.squeeze(inputs)
     inputs = tf.slice(inputs, begin=[0, vertical_slice_size, 0, 0], size=[-1, -1, horizontal_slice_size * 2, -1])
     print(inputs.get_shape())
