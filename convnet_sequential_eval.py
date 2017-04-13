15d4a542041cb078f6439cc81327b49789673a0c
rela1 <ivan.relic1@gmail.com>
2017-04-13 14:53:10 +0200

    changed evaluation data reading

--- convnet_sequential_eval.py
+++ convnet_sequential_eval.py
@@ -60,27 +60,25 @@ def number_of_examples(directory):
   return examples
 
 
-def evaluate(name, sess, logits, loss, tf_records_files, input_placeholder, label_placeholder):
+def evaluate(name, sess, logits, loss, labels, num_examples):
   print("\nRunning evaluation: ", name)
   y_true = []
   y_pred = []
   losses = []
-  for tf_records_file in tf_records_files:
-    for record_string in tf.python_io.tf_record_iterator(tf_records_file, options=tf.python_io.TFRecordOptions(tf.python_io.TFRecordCompressionType.GZIP)):
-        images, labels = parse_example(record_string)
-        images_val, labels_val = sess.run([images, labels])
-        logit_val, loss_val = sess.run([logits, loss], feed_dict={input_placeholder: [images_val], label_placeholder: [labels_val]})
-        pred = np.argmax(logit_val, axis=1)
-        y_pred.append(pred)
-        y_true.append(labels_val)
-        losses.append(loss_val)
+  num_batches = int(math.ceil(num_examples / BATCH_SIZE))
+  for i in range(num_batches):
+    logit_val, loss_val, labels_val = sess.run([logits, loss, labels])
+    pred = np.argmax(logit_val, axis=1)
+    y_pred.extend(pred)
+    y_true.extend(labels_val)
+    losses.append(loss_val)
   metrics = evaluate_default_metric_functions(y_true, y_pred)
   print_metrics(metrics)
-  print('\taverage loss={}'.format(np.mean(losses)))
+  print('\taverage loss={}\n'.format(np.mean(losses)))
   return metrics
 
 
-def evaluate_model(model, dataset_root, model_path):
+def evaluate_trained_model(model, dataset_root, model_path):
   train_dir = os.path.join(dataset_root, 'train')
   valid_dir = os.path.join(dataset_root, 'validate')
   test_dir = os.path.join(dataset_root, 'test')
@@ -95,27 +93,106 @@ def evaluate_model(model, dataset_root, model_path):
     valid_tfrecords = [os.path.join(valid_dir, file) for file in os.listdir(valid_dir)]
     test_tfrecords = [os.path.join(test_dir, file) for file in os.listdir(test_dir)]
 
-    input_placeholder = tf.placeholder(tf.float32, shape=[None] + INPUT_SHAPE)
-    label_placeholder = tf.placeholder(tf.int64, shape=(None, ))
+    train_file_queue = tf.train.string_input_producer(train_tfrecords, num_epochs=EPOCHS)
+    valid_file_queue = tf.train.string_input_producer(valid_tfrecords)
+    test_file_queue = tf.train.string_input_producer(test_tfrecords)
+
+    print('Train tfrecords: {}, valid tfrecords: {}, test tfrecords: {}'.format(train_tfrecords, valid_tfrecords, test_tfrecords))
+    print('Train num examples: {}, valid num examples: {}, test num examples: {}'.format(train_examples, valid_examples, test_examples))
+
+    train_images, train_labels = input_decoder(train_file_queue)
+    train_images, train_labels = tf.train.batch(
+        [train_images, train_labels], batch_size=BATCH_SIZE, shapes=SHAPES, allow_smaller_final_batch=True)
+
+    valid_images, valid_labels = input_decoder(valid_file_queue)
+    valid_images, valid_labels = tf.train.batch(
+        [valid_images, valid_labels], batch_size=BATCH_SIZE, shapes=SHAPES, allow_smaller_final_batch=True)
+
+    test_images, test_labels = input_decoder(test_file_queue)
+    test_images, test_labels = tf.train.batch(
+      [test_images, test_labels], batch_size=BATCH_SIZE, shapes=SHAPES, allow_smaller_final_batch=True)
+
+    train_input_placeholder = tf.placeholder_with_default(train_images, shape=[None] + INPUT_SHAPE)
+    train_label_placeholder = tf.placeholder_with_default(train_labels, shape=(None, ))
+
+    valid_input_placeholder = tf.placeholder_with_default(valid_images, shape=[None] + INPUT_SHAPE)
+    valid_label_placeholder = tf.placeholder_with_default(valid_labels, shape=(None, ))
+
+    test_input_placeholder = tf.placeholder_with_default(test_images, shape=[None] + INPUT_SHAPE)
+    test_label_placeholder = tf.placeholder_with_default(test_labels, shape=(None, ))
 
     sess = tf.Session()
+    global_step = tf.get_variable('global_step', [], dtype=tf.int64,
+        initializer=tf.constant_initializer(0), trainable=False)
 
     with tf.variable_scope('model'):
-      logit_eval, loss_eval = model.build_sequential(input_placeholder, label_placeholder, fully_connected=FULLY_CONNECTED, is_training=False)
+      logit_train, loss_train, init_op = model.build_sequential(train_input_placeholder, train_label_placeholder, fully_connected=FULLY_CONNECTED, is_training=False)
+      logit_valid, loss_valid = model.build_sequential(valid_input_placeholder, valid_label_placeholder, fully_connected=FULLY_CONNECTED, is_training=False)
+      logit_test, loss_test = model.build_sequential(test_input_placeholder, test_label_placeholder, fully_connected=FULLY_CONNECTED, is_training=False)
 
     sess.run(tf.initialize_all_variables())
     sess.run(tf.initialize_local_variables())
-
+    
     saver = tf.train.Saver()
     saver.restore(sess, model_path)
-    evaluate('Train', sess, logit_eval, loss_eval, train_tfrecords, input_placeholder, label_placeholder)
-    evaluate('Validate', sess, logit_eval, loss_eval, valid_tfrecords, input_placeholder, label_placeholder)
-    evaluate('Test', sess, logit_eval, loss_eval, test_tfrecords, input_placeholder, label_placeholder)
-    
+
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+    evaluate('Train', sess, logit_train, loss_train, train_labels, train_examples)
+    evaluate('Validate', sess, logit_valid, loss_valid, valid_labels, valid_examples)
+    evaluate('Test', sess, logit_test, loss_test, test_labels, test_examples)
+
+    coord.request_stop()
+    coord.join(threads)
+    sess.close()
+
+    best_accuracy = 0
+    saver = tf.train.Saver()
+
+    losses = []
+    correct = 0
+    total = 0
+    step = 0
+    best_valid_accuracy = 0
+    start_time = time.time()
+    try:
+      while not coord.should_stop():
+
+          _, logits_val, loss_val, labels_val = sess.run([train_op, logit, loss, train_labels])
+          correct += np.sum(np.argmax(logits_val, axis=1) == labels_val)
+          total += BATCH_SIZE
+          assert not np.isnan(loss_val), 'Model diverged with loss = NaN'
+          losses.append(loss_val)
+          step += 1
+
+          if not step % INFO_STEP:
+            duration = time.time() - start_time
+            print('Average loss: {}, average accuracy: {}, examples/sec: {}, sec/step: {}'.format(np.mean(losses), correct / total, INFO_STEP * BATCH_SIZE / duration, float(duration)))
+            start_time = time.time()
+            losses.clear()
+            correct = 0
+            total = 0
+
+          if (step * BATCH_SIZE) >= train_examples:
+            step = 0
+            metrics = evaluate('Validation', sess, logit_eval, loss_eval, valid_labels, valid_examples)
+            if metrics['accuracy_score'] > best_valid_accuracy:
+              best_valid_accuracy = metrics['accuracy_score']
+              print('\tNew best validation accuracy', best_valid_accuracy)
+              saver.save(sess, model_path)
+    except tf.errors.OutOfRangeError:
+      print('Done training -- epoch limit reached')
+      saver.restore(sess, model_path)
+      evaluate('Test', sess, logit_test, loss_test, test_labels, test_examples)
+    finally:
+      coord.request_stop()
+
+    coord.join(threads)
     sess.close()
 
 
 if __name__ == '__main__':
   dataset_root = sys.argv[1]
   model_path = sys.argv[2]
-  evaluate_model(vgg_vertically_sliced, dataset_root, model_path)
+  evaluate_trained_model(vgg_vertically_sliced, dataset_root, model_path)
