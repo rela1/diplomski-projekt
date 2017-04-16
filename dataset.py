63f561686514097cdd58b8622af9c4ecd26bb3bd
rela1 <ivan.relic1@gmail.com>
2017-04-16 17:00:33 +0200

    changed way of dataset loading

--- dataset.py
+++ dataset.py
@@ -32,10 +32,8 @@ class Dataset:
 
         train_images, train_labels = input_decoder(train_file_queue, example_parser)
         if is_training:
-            min_after_dequeue = 10000
-            capacity = min_after_dequeue + 3 * batch_size
             self.train_images, self.train_labels = tf.train.shuffle_batch(
-                [train_images, train_labels], batch_size=batch_size, shapes=shapes, allow_smaller_final_batch=True, capacity=capacity, min_after_dequeue=min_after_dequeue)
+                [train_images, train_labels], batch_size=batch_size, shapes=shapes, allow_smaller_final_batch=True, capacity=2000, min_after_dequeue=1000)
         else:
             self.train_images, self.train_labels = tf.train.batch(
                 [train_images, train_labels], batch_size=batch_size, shapes=shapes, allow_smaller_final_batch=True)
