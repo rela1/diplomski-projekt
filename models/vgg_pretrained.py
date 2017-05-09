7b77f494245fc3605b977591ed9dee0a0b7b2ae8
rela1 <ivan.relic1@gmail.com>
2017-05-10 00:15:55 +0200

    removed batchnorm after vgg conv

--- models/vgg_pretrained.py
+++ models/vgg_pretrained.py
@@ -72,6 +72,7 @@ class SequentialImageLSTMModel:
         net = layers.convolution2d(net, 512, scope='conv5_1', reuse=reuse)
         net = layers.convolution2d(net, 512, scope='conv5_2', reuse=reuse)
         net = layers.convolution2d(net, 512, scope='conv5_3', reuse=reuse)
+        net = layers.max_pool2d(net, 2, 2, scope='pool5')
 
         """
         net = layers.batch_norm(net, decay=bn_params['decay'], center=bn_params['center'], 
@@ -186,13 +187,14 @@ class SequentialImageTemporalFCModel:
         net = layers.convolution2d(net, 512, scope='conv5_1', reuse=reuse)
         net = layers.convolution2d(net, 512, scope='conv5_2', reuse=reuse)
         net = layers.convolution2d(net, 512, scope='conv5_3', reuse=reuse)
+        net = layers.max_pool2d(net, 2, 2, scope='pool5')
 
+        """
         net = layers.batch_norm(net, decay=bn_params['decay'], center=bn_params['center'], 
                 scale=bn_params['scale'], epsilon=bn_params['epsilon'], 
                 updates_collections=bn_params['updates_collections'], is_training=bn_params['is_training'],
                 scope='batch_norm', reuse=reuse)
-
-        net = layers.max_pool2d(net, 2, 2, scope='pool5')
+        """
 
       """
       net_shape = net.get_shape()
@@ -308,11 +310,14 @@ class SequentialImagePoolingModel:
         net = layers.convolution2d(net, 512, scope='conv5_1', reuse=reuse)
         net = layers.convolution2d(net, 512, scope='conv5_2', reuse=reuse)
         net = layers.convolution2d(net, 512, scope='conv5_3', reuse=reuse)
+        net = layers.max_pool2d(net, 2, 2, scope='pool5')
 
+        """
         net = layers.batch_norm(net, decay=bn_params['decay'], center=bn_params['center'], 
                 scale=bn_params['scale'], epsilon=bn_params['epsilon'], 
                 updates_collections=bn_params['updates_collections'], is_training=bn_params['is_training'],
                 scope='batch_norm', reuse=reuse)
+        """
 
       if concated is None:
         concated = tf.expand_dims(net, axis=1)
