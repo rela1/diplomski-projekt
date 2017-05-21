285606ed7c8d3ed6eceef352d8b919fff5c19e34
rela1 <ivan.relic1@gmail.com>
2017-05-21 11:57:34 +0200

    dataset normalization debug

--- dataset.py
+++ dataset.py
@@ -58,10 +58,9 @@ class Dataset:
         for i in range(num_batches):
           print('Normalization step {}/{}'.format(i + 1, num_batches))
           image_vals = sess.run(self.train_images)
+          print(image_vals.shape)
           for j in range(len(image_vals)):
             np.add(mean_image, image_vals[j], mean_image)
-          if not i % 5:
-            gc.collect()
         np.divide(mean_image, float(self.num_train_examples), mean_image)
         tf_mean_image = tf.constant(mean_image, dtype=tf.float32)
         self.train_images = tf.subtract(self.train_images, tf_mean_image, name='train_images_mean_image_normalization')
