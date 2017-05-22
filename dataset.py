3f920a90b0d0398aa15d3129e693b4d85a5e89d7
rela1 <ivan.relic1@gmail.com>
2017-05-22 14:13:32 +0200

    queue capacity change

--- dataset.py
+++ dataset.py
@@ -37,7 +37,7 @@ class Dataset:
         train_images, train_labels = input_decoder(train_file_queue, example_parser)
         if is_training:
             self.train_images, self.train_labels = tf.train.shuffle_batch(
-                [train_images, train_labels], batch_size=batch_size, shapes=shapes, allow_smaller_final_batch=True, num_threads=2, capacity=50, min_after_dequeue=25)
+                [train_images, train_labels], batch_size=batch_size, shapes=shapes, allow_smaller_final_batch=True, num_threads=2, capacity=100, min_after_dequeue=75)
         else:
             self.train_images, self.train_labels = tf.train.batch(
                 [train_images, train_labels], batch_size=batch_size, shapes=shapes, allow_smaller_final_batch=True, num_threads=2)
