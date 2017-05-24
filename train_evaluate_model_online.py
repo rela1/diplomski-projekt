1c71f8b22f05d45abc34e3d0f6e6609aa7ff2b7a
rela1 <ivan.relic1@gmail.com>
2017-05-25 01:24:53 +0200

    tensor shape fix

--- train_evaluate_model_online.py
+++ train_evaluate_model_online.py
@@ -96,7 +96,7 @@ def evaluate(dataset_name, sess, sequence_length, spatials_model, temporal_model
     y_prob.extend(probs_val)
   for t in range(sequence_length - 1, dataset.num_train_examples):
     representation_t = spatials_model.forward(sess, t % sequence_length)
-    logits, labels_val = temporal_model.forward(sess, representation_t)
+    logits_val, labels_val = temporal_model.forward(sess, representation_t)
     preds_val = np.argmax(logits, axis=1)
     probs_val = softmax(logits_val)
     y_pred.extend(preds_val)
