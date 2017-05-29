c2d9f6408db8f7a475e4bc4dcee5e5b56ebca7a9
rela1 <ivan.relic1@gmail.com>
2017-05-29 13:23:14 +0200

    convolution training preloads trained model

--- models/vgg_pretrained.py
+++ models/vgg_pretrained.py
@@ -172,7 +172,7 @@ class SequentialImageTemporalFCModelOnline:
         net = layers.batch_norm(net, decay=bn_params['decay'], center=bn_params['center'], 
           scale=bn_params['scale'], epsilon=bn_params['epsilon'], 
           updates_collections=bn_params['updates_collections'], is_training=bn_params['is_training'],
-          scope='batch_norm', reuse=reuse)
+          scope='batch_norm')
 
       net_shape = net.get_shape()
 
