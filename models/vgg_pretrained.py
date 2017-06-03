825b20d3d6b89c3959a54c4fa16b0978d3f48998
rela1 <ivan.relic1@gmail.com>
2017-06-03 03:20:57 +0200

    changed temporal FC architecture

--- models/vgg_pretrained.py
+++ models/vgg_pretrained.py
@@ -373,16 +373,19 @@ class SequentialImageTemporalFCModel:
 
       net_shape = net.get_shape()
 
+      global_pooling_kernel = [int(net_shape[1]), int(net_shape[2])]
+      net = layers.max_pool2d(net, kernel_size=global_pooling_kernel, stride=global_pooling_kernel, scope='global_pool1')
+
+      net_shape = net.get_shape()
+
       net = tf.reshape(net, [batch_size, int(net_shape[1]) * int(net_shape[2]) * int(net_shape[3])])
       
-      """
       with tf.contrib.framework.arg_scope([layers.fully_connected],
         activation_fn=tf.nn.relu, normalizer_fn=layers.batch_norm, normalizer_params=bn_params,
         weights_initializer=layers.variance_scaling_initializer(),
         weights_regularizer=layers.l2_regularizer(weight_decay)):
         net = layers.fully_connected(net, spatial_fully_connected_size, scope='spatial_FC', reuse=reuse)
         net = layers.dropout(net, keep_prob=DROPOUT_KEEP_PROB, is_training=is_training, scope='spatial_FC_dropout')
-      """
 
       if concated is None:
         concated = tf.expand_dims(net, axis=1)
