9a69400ddec5c6c7793bdbc0a727481e1942a91a
rela1 <ivan.relic1@gmail.com>
2017-06-06 18:02:12 +0200

    wrong classification plot

--- models/vgg_pretrained.py
+++ models/vgg_pretrained.py
@@ -336,6 +336,7 @@ class SequentialImageTemporalFCModel:
     vertical_slice_size = int(round(int(inputs_shape[2]) / 3))
     inputs = tf.slice(inputs, begin=[0, 0, vertical_slice_size, 0, 0], size=[-1, -1, -1, horizontal_slice_size * 2, -1])
 
+    print(inputs.get_shape())
 
     concated = None
 
@@ -373,6 +374,7 @@ class SequentialImageTemporalFCModel:
         """
 
       net_shape = net.get_shape()
+      print(net_shape)
 
       net = tf.reshape(net, [batch_size, int(net_shape[1]) * int(net_shape[2]) * int(net_shape[3])])
       
