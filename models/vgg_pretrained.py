1fb8864fe9bf8a4afd18c92c2b19a3326940b460
rela1 <ivan.relic1@gmail.com>
2017-06-06 17:57:04 +0200

    wrong classification plot

--- models/vgg_pretrained.py
+++ models/vgg_pretrained.py
@@ -384,7 +384,7 @@ class SequentialImageTemporalFCModel:
         activation_fn=tf.nn.relu, normalizer_fn=layers.batch_norm, normalizer_params=bn_params,
         weights_initializer=layers.variance_scaling_initializer(),
         weights_regularizer=layers.l2_regularizer(weight_decay)):
-        net = layers.fully_connected(net, spatial_fully_connected_size, scope='spatial_FC')
+        net = layers.fully_connected(net, spatial_fully_connected_size, scope='spatial_FC', reuse=reuse)
         #net = layers.dropout(net, keep_prob=DROPOUT_KEEP_PROB, is_training=is_training, scope='spatial_FC_dropout{}'.format(layer_num))
 
       if concated is None:
