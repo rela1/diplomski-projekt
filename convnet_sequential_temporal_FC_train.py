46909d4f0240a82596c7c75cd785002d54344bbf
rela1 <ivan.relic1@gmail.com>
2017-06-08 12:04:45 +0200

    temporal fc variable batch size

--- convnet_sequential_temporal_FC_train.py
+++ convnet_sequential_temporal_FC_train.py
@@ -10,7 +10,6 @@ LEARNING_RATE = 5e-4
 SPATIAL_FULLY_CONNECTED = [128, 64]
 TEMPORAL_FULLY_CONNECTED = [128, 64]
 EPOCHS = 10
-BATCH_SIZE = 2
 INPUT_SHAPE = [25, 140, 350, 3]
 
 
@@ -18,8 +17,9 @@ if __name__ == '__main__':
   vgg_init_dir = sys.argv[1]
   dataset_root = sys.argv[2]
   model_path = sys.argv[3]
+  batch_size = int(sys.argv[4])
 
-  dataset = ImageSequenceDataset(dataset_root, BATCH_SIZE, INPUT_SHAPE, is_training=True)
+  dataset = ImageSequenceDataset(dataset_root, batch_size, INPUT_SHAPE, is_training=True)
 
   model = SequentialImageTemporalFCModel(SPATIAL_FULLY_CONNECTED, TEMPORAL_FULLY_CONNECTED, dataset, weight_decay=WEIGHT_DECAY, vgg_init_dir=vgg_init_dir, is_training=True)
 
