f6fdbc893699762c99dd80b3cca624844df00b62
rela1 <ivan.relic1@gmail.com>
2017-06-12 02:11:21 +0200

    add combined convolutional training

--- dataset.py
+++ dataset.py
@@ -118,7 +118,7 @@ class CombinedImageSequenceDataset(Dataset):
         self.positive_sequences_dirs_test = self.get_positive_sequences_dirs([os.path.join(test_tfrecords_dir, 'positives') for test_tfrecords_dir in test_tfrecords_dirs])
 
     def get_positive_sequences_dirs(self, tfrecords_dirs):
-        positives_dirs = [os.path.join(train_tfrecords_dir, 'positives') for train_tfrecords_dir in train_tfrecords_dirs]
+        positives_dirs = [os.path.join(tfrecords_dir, 'positives') for tfrecords_dir in tfrecords_dirs]
         positives_sequences_dirs = []
         for positives_dir in positives_dirs:
             positives_dir_sequences = os.listdir(positives_dir)
