2311232a52fdd5b37e8730df0237908dd4effcfa
rela1 <ivan.relic1@gmail.com>
2017-06-19 16:21:20 +0200

    temporal pooling model

--- convnet_sequential_pooling_train.py
+++ convnet_sequential_pooling_train.py
@@ -9,7 +9,6 @@ WEIGHT_DECAY = 5e-4
 LEARNING_RATE = 1e-4
 FULLY_CONNECTED = [200]
 EPOCHS = 10
-BATCH_SIZE = 10
 INPUT_SHAPE = [25, 40, 100, 3]
 
 
@@ -19,7 +18,7 @@ if __name__ == '__main__':
   model_path = sys.argv[3]
   batch_size = int(sys.argv[4])
 
-  dataset = ImageSequenceDataset(dataset_root, BATCH_SIZE, INPUT_SHAPE, is_training=True)
+  dataset = ImageSequenceDataset(dataset_root, batch_size, INPUT_SHAPE, is_training=True)
 
   model = SequentialImagePoolingModel(FULLY_CONNECTED, dataset, weight_decay=WEIGHT_DECAY, vgg_init_dir=vgg_init_dir, is_training=True)
 
