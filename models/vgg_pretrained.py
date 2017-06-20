5266869944f8e3cc768927524b99e31eb2f78f39
rela1 <ivan.relic1@gmail.com>
2017-06-20 03:58:31 +0200

    temporal pooling model

--- models/vgg_pretrained.py
+++ models/vgg_pretrained.py
@@ -444,6 +444,7 @@ class SequentialImagePoolingModel:
     horizontal_slice_size = int(round(int(inputs_shape[3]) / 3))
     vertical_slice_size = int(round(int(inputs_shape[2]) / 3))
     inputs = tf.slice(inputs, begin=[0, 0, vertical_slice_size, 0, 0], size=[-1, -1, -1, horizontal_slice_size * 2, -1])
+    print(inputs.get_shape())
 
     reuse = None
     for sequence_image in range(int(inputs_shape[1])):
