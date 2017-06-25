540e3a6b57ed97a3fe60d5e9bea49fb1c25df4ae
rela1 <ivan.relic1@gmail.com>
2017-06-25 23:13:11 +0200

    add sequential pooling eval

---
+++ convnet_sequential_pooling_eval.py
@@ -0,0 +1,22 @@
+import sys
+
+from dataset import ImageSequenceDataset
+from models.vgg_pretrained import SequentialImagePoolingModel
+from train_evaluate_model import freezed_pretrained_train_model
+
+
+WEIGHT_DECAY = 5e-4
+FULLY_CONNECTED = [200]
+INPUT_SHAPE = [25, 140, 350, 3]
+BATCH_SIZE = 2
+
+
+if __name__ == '__main__':
+  dataset_root = sys.argv[1]
+  model_path = sys.argv[2]
+
+  dataset = ImageSequenceDataset(dataset_root, BATCH_SIZE, INPUT_SHAPE, is_training=False)
+
+  model = SequentialImagePoolingModel(FULLY_CONNECTED, dataset, weight_decay=WEIGHT_DECAY, is_training=False)
+
+  evaluate_model(model, dataset, model_path)
