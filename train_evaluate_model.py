e37793514302601aa1af8795e2b925a80f83b7b3
rela1 <ivan.relic1@gmail.com>
2017-07-04 20:02:33 +0200

    add geoinformation to wrong classifications

--- train_evaluate_model.py
+++ train_evaluate_model.py
@@ -174,7 +174,7 @@ def plot_wrong_classifications(model, dataset, model_path, save_path=None):
   sess.run(tf.global_variables_initializer())
   sess.run(tf.local_variables_initializer())
     
-  saver = tf.train.Saver()
+  saver = tf.train.Saver(get_saver_variables())
   saver.restore(sess, model_path)
 
   coord = tf.train.Coordinator()
